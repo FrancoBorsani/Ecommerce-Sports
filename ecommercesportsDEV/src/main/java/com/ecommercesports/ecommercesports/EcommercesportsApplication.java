440a30e6b277124e60b3db6f3cc31a2727fc13e3
unknown <borsafranco@gmail.com>
2020-09-02 17:04:38 -0300

    Proyecto base Spring Boot con dependencias

---
+++ ecommercesportsDEV/src/main/java/com/ecommercesports/ecommercesports/EcommercesportsApplication.java
@@ -0,0 +1,13 @@
+package com.ecommercesports.ecommercesports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercesportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercesportsApplication.class, args);
+	}
+
+}
