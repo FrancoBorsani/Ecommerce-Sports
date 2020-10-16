e6614d88579524a187e100d8f871cdf9d3869395
unknown <borsafranco@gmail.com>
2020-10-15 22:50:02 -0300

    Backend del formulario de contacto y mail enviado a la administración del ecommerce

--- ecommercesportsDEV/src/main/java/com/ecommercesports/ecommercesports/controllers/HomeController.java
+++ ecommercesportsDEV/src/main/java/com/ecommercesports/ecommercesports/controllers/HomeController.java
@@ -1,10 +1,14 @@
 package com.ecommercesports.ecommercesports.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ecommercesports.ecommercesports.helpers.ViewRouteHelpers;
+import com.ecommercesports.ecommercesports.implementation.SendMailService;
 
 @RequestMapping("/")
 @Controller
@@ -24,5 +28,25 @@ public class HomeController {
 	public String info_Institucional() {
 		return ViewRouteHelpers.INFO;
 	}
+	
+	 @Autowired
+	    private SendMailService SendmailService;
+	
+	@PostMapping("/contactar")
+	public String contactar(@RequestParam("nombre") String nombre, @RequestParam("mail") String mail, @RequestParam("consulta") String consulta) {
+	String subject = "CONSULTA EN ECOMMERCE-SPORTS";
+	     
+	 String message = "\nMensaje de: " + nombre + "\nMail de contacto: " + mail  + "\nMensaje: " + consulta;
+     SendmailService.sendMail("proyectodesoftwaretp@gmail.com", ""+ mail, subject,message);
+    
+		
+		
+		
+		return ViewRouteHelpers.HOME;
+	}
+	
+	
+	
+	
 	
 }
